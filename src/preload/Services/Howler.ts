b3e51c88a3b0e6a0bd8885378505acd38bfc8f10
Marcelo Gomes <marcelo.gomj@gmail.com>
2023-11-27 19:34:07 -0300

    add: auto next musics

--- src/preload/Services/Howler.ts
+++ src/preload/Services/Howler.ts
@@ -1,8 +1,8 @@
 import { Howl } from "howler";
 // import store from "./ElectronStore";
 
-type StateFunctionProp <T> = ((arg: T) => void | number ) ;
-type HandlerHowl <T> = ((howl: Howl, state : StateFunctionProp<T>) => void) | null;
+type StateFunctionProp <T> = ((arg: T) => (T | void))
+type HandlerHowl <T> = (howl: Howl, state : StateFunctionProp<T>) => T;
 type UpdateCurrentMusic = { 
   currentMusicId ?: number, 
   updateQueueMusics ?: string[] 
@@ -11,7 +11,8 @@ type UpdateCurrentMusic = {
 function playContextAudio( 
   queueMusics: string[], 
   initialCurrentMusic: number, 
-  setNextMusicQueue: StateFunctionProp<number | null> 
+  setNextMusicQueue: StateFunctionProp<number>,
+  setHandleGlobal: StateFunctionProp<any>
 ) {
   const currentMusic = queueMusics[initialCurrentMusic];
   // create context of music
@@ -21,14 +22,30 @@ function playContextAudio(
     volume: 0.05
   });
 
-  howlContext.play()
+  howlContext.play();
+
+  howlContext.once('end', () => {
+    howlContext.unload();
+    
+    const nextMusicIndex = initialCurrentMusic += 1;
+    setNextMusicQueue(nextMusicIndex);
+    const autoUpdateHowler = playContextAudio(
+      queueMusics, 
+      nextMusicIndex, 
+      setNextMusicQueue, 
+      setHandleGlobal
+    ) 
+    
+    setHandleGlobal(() => autoUpdateHowler)
+  })
 
   return function howlerHandler<T>(
     { currentMusicId, updateQueueMusics } : UpdateCurrentMusic,
     func ?: HandlerHowl<T>, 
-    updateState ?: StateFunctionProp<T | typeof howlerHandler >,
+    updateState ?: StateFunctionProp<T>,
   ){
     if(func) {
+      // get access the current music info
       func(howlContext, updateState || (() => {}))
       return;
     }
@@ -40,11 +57,13 @@ function playContextAudio(
       
       const context = playContextAudio(
         updateQueueMusics || queueMusics, 
-        currentMusicId, setNextMusicQueue
+        currentMusicId, 
+        setNextMusicQueue,
+        setHandleGlobal
       );      
 
       // update howler that handle current state of music
-      updateState(() => context)
+      updateState(<T>(() => context))
     }
   }
 }
@@ -54,7 +73,6 @@ function currentDuration(howl: Howl, setDuration: StateFunctionProp<number>){
     howl.seek()
   )
 }
-
 
 function setProgressMusic(howl: Howl, setProgress: StateFunctionProp<number>) {
   const progress = setProgress(0);
